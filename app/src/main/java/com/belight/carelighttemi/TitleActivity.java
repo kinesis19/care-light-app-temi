017e6d9a579ab43ba6bb7323a1222be78f487879
kinesis19 <lordkinesis@naver.com>
2025-06-06 22:04:29 +0900

    :sparkles: Feat: start Login Activity feature

--- app/src/main/java/com/belight/carelighttemi/TitleActivity.java
+++ app/src/main/java/com/belight/carelighttemi/TitleActivity.java
@@ -1,6 +1,10 @@
 package com.belight.carelighttemi;
 
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,7 +12,13 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+
+
 public class TitleActivity extends AppCompatActivity {
+
+
+    private static final String TAG = "TitleActivity";
+    private static final int SPLASH_TIMEOUT = 2000; // 2초 (2000ms)
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +30,16 @@ public class TitleActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        // 2초 후에 실행될 로직 추가
+        new Handler(Looper.getMainLooper()).postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                // LoginActivity로 이동하기 위한 Intent 생성
+                Intent intent = new Intent(TitleActivity.this, LoginActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, SPLASH_TIMEOUT);
     }
 }
